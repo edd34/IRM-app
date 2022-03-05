b51d76f0989924321d10f1fb7c4967e9e989a0ea
edd34 <eddine.farid@hotmail.fr>
2022-03-06 01:11:31 +0300

    run black on files

--- components/traffic_alert/models.py
+++ components/traffic_alert/models.py
@@ -7,20 +7,12 @@ from components.localisation.models import Localisation
 
 
 class TrafficAlert(models.Model):
-    UUID = models.UUIDField(
-        unique=True, primary_key=True, default=uuid.uuid4, editable=False
-    )  # make sure unique uuid
+    UUID = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)  # make sure unique uuid
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     timestamp = models.DateTimeField(auto_now=True)
     localisation_description = models.CharField(max_length=150, blank=True, null=True)
-    report_description = models.CharField(
-        max_length=150, blank=True, null=True
-    )  # string : optional
-    localisation_id = models.ForeignKey(
-        Localisation, on_delete=models.CASCADE, null=True, blank=True
-    )
-    alert_id = models.ForeignKey(
-        AlertTable, on_delete=models.CASCADE, null=False, blank=False, default=1
-    )
+    report_description = models.CharField(max_length=150, blank=True, null=True)  # string : optional
+    localisation_id = models.ForeignKey(Localisation, on_delete=models.CASCADE, null=True, blank=True)
+    alert_id = models.ForeignKey(AlertTable, on_delete=models.CASCADE, null=False, blank=False, default=1)
     # TODO reportByMunicipalityUser = relation to user model

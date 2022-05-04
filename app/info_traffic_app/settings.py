2dd0403a62442cdb0a11fcb2bfef7e8ac604df54
edd34 <eddine.farid@hotmail.fr>
2022-05-04 22:23:25 +0300

    use database postgresql

--- app/info_traffic_app/settings.py
+++ app/info_traffic_app/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = "info_traffic_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("hello_django_dev"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
